ad8e77a3ff45d1569c41a97473b526cbbd3dcd3d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 20:38:59 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("O que é, o que é?"); 
+Thread.Sleep(5000);
+Console.WriteLine("anda mas nao tem perna e grita mas nao tem boca.");
+Thread.Sleep(5000);
+Console.WriteLine("O carro.");
+
